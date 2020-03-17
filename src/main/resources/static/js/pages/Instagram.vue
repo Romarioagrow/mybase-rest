43a266cc6a5654afa9e83129c92edc55ecb71d6b
Romarioagrow <romarioagrow@mail.ru>
2020-03-17 22:52:25 +0500

    v2.2 Followers Front Loading

--- src/main/resources/static/js/pages/Instagram.vue
+++ src/main/resources/static/js/pages/Instagram.vue
@@ -4,11 +4,6 @@
             <v-row>
                 <v-col>
                     <v-card wight="950" outlined tile>
-                        <!--ВВОД-->
-                        <v-card-title>
-                            Facebook Graph API
-                        </v-card-title>
-
                         <v-card-title>
                             <v-icon class="mr-3">mdi-instagram</v-icon>
                             <span>Instagram&nbsp;</span>
@@ -64,14 +59,20 @@
                         </v-card-actions>
                         <v-divider/>
                         <v-card-actions>
-                            <v-btn outlined @click="loadInstFollowersLists()" class="ml-12">
-                                <div>Last Update: <span>{{calculateLastUpdate(followersData.lastUpdate)}}</span></div>
+                            <v-btn class="ml-12"
+                                   outlined
+                                   @click="loadInstFollowersLists()"
+                                   :color="buttonColor"
+                                   :loading="followersLoading"
+                            >
+                                <div>Last Update: <span>{{lastUpdate}}</span></div>
                             </v-btn>
                         </v-card-actions>
                         <v-divider/>
                         <v-card-actions class="p-0">
                             <v-expansion-panels accordion multiple tile flat>
-                                <v-expansion-panel style="border-bottom: 1px solid #e0e0e0;">
+                                <v-expansion-panel style="border-bottom: 1px solid #e0e0e0;"
+                                                   :disabled="followersLoading">
                                     <v-expansion-panel-header>
                                         <v-row>
                                             <v-col cols="1">
@@ -119,173 +120,12 @@
                                         </v-row>
                                     </v-expansion-panel-content>
                                 </v-expansion-panel>
-                                <v-expansion-panel v-for="(item, i) in 4" :key="i" style="border-bottom: 1px solid #e0e0e0;">
-                                    <v-expansion-panel-header>
-                                        <v-row>
-                                            <v-col>
-                                                Item
-                                            </v-col>
-                                            <v-col>
-                                                Property
-                                            </v-col>
-                                            <v-col>
-                                                Amount
-                                            </v-col>
-                                        </v-row>
-                                    </v-expansion-panel-header>
-                                    <v-expansion-panel-content>
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                    </v-expansion-panel-content>
-                                </v-expansion-panel>
                             </v-expansion-panels>
                         </v-card-actions>
                     </v-card>
-
                 </v-col>
             </v-row>
             <v-spacer/>
-            <!--OPEN API-->
-            <v-row v-if="false"> <!--!!!!!!-->
-                <v-col>
-                    <v-card min-height="850">
-                        <v-card-title>Instagram Open API</v-card-title>
-                        <v-card-actions>
-                            <v-row>
-                                <v-col>
-                                    <v-text-field solo dense v-model="scrapperInstUsername"></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-btn block color="error" @click="loadInstDataFreeAPI()">GO</v-btn>
-                                </v-col>
-                            </v-row>
-                        </v-card-actions>
-                        <v-divider></v-divider>
-                        <v-card-title v-if="!loading">
-                            <span >{{instAccount.username}}</span>
-                        </v-card-title>
-                        <!--PROFILE-->
-                        <div v-if="!loading">
-                            <v-card-text>
-                                <v-row>
-                                    <v-col cols="3">
-                                        <v-card width="150" height="150" @click.stop="dialog = true" hover>
-                                            <v-img contain :src="instAccountPicFull"></v-img>
-                                        </v-card>
-                                        <v-dialog v-model="dialog" max-width="800">
-                                            <v-card>
-                                                <v-img contain :src="instAccountPicFull"></v-img>
-                                                <v-card-actions>
-                                                    <v-btn color="error" block outlined @click="dialog = false">
-                                                        Save
-                                                    </v-btn>
-                                                </v-card-actions>
-                                            </v-card>
-                                        </v-dialog>
-                                    </v-col>
-                                    <v-col cols="4">
-                                        <v-row>
-                                            <v-col>
-                                                <div class="headline font-weight-thin">{{instAccount.fullName}}</div>
-                                            </v-col>
-                                        </v-row>
-                                        <v-row>
-                                            <v-col >
-                                                <v-btn :disabled="false" outlined>Followers:
-                                                    {{instAccount.followedBy}}
-                                                    <v-icon v-if="!followsLoading">mdi-arrow-up</v-icon>
-                                                </v-btn>
-                                            </v-col>
-                                        </v-row>
-                                        <v-row>
-                                            <v-col>
-                                                <v-btn :disabled="false" outlined>Following:
-                                                    {{instAccount.follows}}
-                                                    <v-icon v-if="!followsLoading">mdi-arrow-up</v-icon>
-                                                </v-btn>
-                                            </v-col>
-                                        </v-row>
-                                    </v-col>
-                                    <v-col>
-                                        <v-row>
-                                            <v-col>
-                                                <div class="title font-weight-light">{{instAccount.biography}}</div>
-                                            </v-col>
-                                        </v-row>
-                                        <v-row>
-                                            <v-col>
-                                                <div class="subtitle-2">{{instAccount.externalUrl}}</div>
-                                            </v-col>
-                                        </v-row>
-                                    </v-col>
-                                </v-row>
-                            </v-card-text>
-                        </div>
-                        <div v-else class="mx-auto">
-                            <v-card-actions>
-                                <v-progress-circular
-                                        style="margin-left: 45%"
-                                        :size="70"
-                                        :width="7"
-                                        color="purple"
-                                        indeterminate
-                                ></v-progress-circular>
-                            </v-card-actions>
-                        </div>
-                        <v-divider></v-divider>
-                        <!--POSTS-->
-                        <v-container>
-                            <v-row>
-                                <v-col>Всего постов: {{totalPosts}} </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col v-for="instPost in instPosts" :key="instPost.id">
-                                    <v-card class="d-inline-block mx-auto">
-                                        <v-container>
-                                            <v-row>
-                                                <v-col>
-                                                    <v-img height="200" width="200" :src="instPost.displayUrl"></v-img>
-                                                    <!--<v-img contain :src="instAccountPicFull"></v-img>-->
-                                                    <!-- <v-img height="200" width="200" :src="instPost.displayUrl" @click="postDialog = true"></v-img>-->
-                                                    <!--<v-dialog v-model="postDialog" max-width="800">
-                                                        <v-card>
-                                                            <v-img contain :src="instPost.displayUrl"></v-img>
-                                                            <v-card-actions>
-                                                                <v-btn color="error" block outlined @click="postDialog = false">
-                                                                    Save
-                                                                </v-btn>
-                                                            </v-card-actions>
-                                                        </v-card>
-                                                    </v-dialog>-->
-                                                </v-col>
-                                                <v-col>
-                                                    <v-row>
-                                                        <v-col>
-                                                            <v-btn icon>
-                                                                <v-icon>mdi-heart</v-icon>
-                                                                {{instPost.likeCount}}
-                                                            </v-btn>
-                                                        </v-col>
-                                                        <v-col>
-                                                            <v-btn icon>
-                                                                <v-icon>mdi-bookmark</v-icon>
-                                                            </v-btn>
-                                                        </v-col>
-                                                        <v-col>
-                                                            <v-btn icon>
-                                                                <v-icon>mdi-share-variant</v-icon>
-                                                            </v-btn>
-                                                        </v-col>
-                                                    </v-row>
-                                                </v-col>
-                                            </v-row>
-                                        </v-container>
-                                    </v-card>
-                                </v-col>
-                            </v-row>
-                        </v-container>
-                    </v-card>
-                </v-col>
-            </v-row>
         </b-container>
     </v-content>
 </template>
@@ -303,9 +143,21 @@
     });
     import FollowerChip from "components/FollowerChip.vue";
     export default {
+
+
+        /*!!!!!!!!!
+        *
+        * ПРИРОСТ FOLLOWERS FOR/SINCE LAST UPDATE
+        *
+        *
+        * */
+
+
         components: {FollowerChip},
         data() {
             return {
+                followersLoading: true,
+                buttonColor: 'error',
 
                 instAccount: {},
                 instUsername: 'romarioagrow',
@@ -332,6 +184,13 @@
             this.loadInstagramPage()
         },
         computed: {
+            lastUpdate() {
+                /*let dataTime = lastUpdate.substr(0, lastUpdate.lastIndexOf('.'))
+                return dataTime.replace('-','.').replace('-','.').replace('T',' at ')*/
+                const update = this.followersData.lastUpdate
+                return update ? this.calculateLastUpdate(update) : 'X'
+            },
+
             instProfile() {
                 return this.$store.state.instProfile
             },
@@ -341,9 +200,7 @@
         },
         methods: {
             async loadInstagramPage() {
-                //this.loadCrapperInstAccount()
                 await this.loadInstAccountGraphAPI()
-
                 await this.loadInstFollowersListsDB()
             },
             /**/
@@ -363,15 +220,18 @@
                 {
                     this.followersData = instFollowers
                     console.log('followersData LOADED FROM STORAGE: ' + this.followersData)
+                    this.followersLoading = false
                 }
                 else
                 {
                     console.log('followersData LOADING FROM API')
                     const url = '/api/social/instagram/graph/loadInstFollowersListsDB'
-                    this.loadInstFollowersLists(url)
+                    this.loadInstFollowersLists(url, 'no_color')
                 }
             },
-            loadInstFollowersLists(url) {
+            async loadInstFollowersLists(url, no_color) {
+                this.followersLoading = true
+
                 let requestURL = url ? url : '/api/social/instagram/graph/loadInstFollowersData'
                 console.log('loadInstFollowersLists')
                 console.log('url: ' + requestURL)
@@ -381,12 +241,16 @@
                     'username': this.instUsername,
                     'sessionid': this.sessionid
                 }
-                axios.post(requestURL, dataToServer, configJson).then(response => {
+                await axios.post(requestURL, dataToServer, configJson).then(response => {
                     console.log(response.data.lostFollowersAmount)
 
                     this.followersData = response.data
                     this.$store.dispatch('loadInstDataToStorage', response.data)
+                    this.followersLoading = false
                 })
+
+                this.followersLoading = false
+                if (!no_color) this.buttonColor = 'success'
             },
             /**/
 
