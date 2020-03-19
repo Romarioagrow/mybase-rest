b98cc3859a98b6e9ad7dc21cb9b2184d92416f0b
Romarioagrow <romarioagrow@mail.ru>
2020-03-19 23:37:09 +0500

    v3.0 Facebook Full Auth (Reload)

--- src/main/resources/static/js/store.js
+++ src/main/resources/static/js/store.js
@@ -8,27 +8,16 @@ import router from './router'
 
 export default new Vuex.Store({
     state: {
-        /*currentUser: null,*/
-
         instProfile: null,
         instFollowers: null
-
-        //newFollowersData: null,
     },
     mutations: {
+
         setInstFollowers(currentState, instFollowers) {
             currentState.instFollowers = instFollowers
         },
-
-        /*addNewFollowersData(currentState, newFollowersData) {
-            currentState.newFollowersData = newFollowersData
-        },*/
-
         setInstProfile(currentState, profile) {
             currentState.instProfile = profile
-        },
-        setCurrentUser(currentState, user) {
-            currentState.currentUser = user
         },
         logoutUser(currentState) {
             currentState.instProfile = null
@@ -38,17 +27,12 @@ export default new Vuex.Store({
     actions: {
         async doLogout(context) {
             context.commit('logoutUser')
-            //await axios.post('https://localhost:8080/logout')
+            window.location.href = 'https://localhost:8080/logout'
         },
-
 
         loadInstDataToStorage(context, instFollowers) {
             context.commit('setInstFollowers', instFollowers)
         },
-
-        /*newFollowersData(context, user) {
-            context.commit('addNewFollowersData', user)
-        },*/
 
         loadInstUserProfile(context, instProfile) {
             context.commit('setInstProfile', instProfile)
@@ -57,16 +41,8 @@ export default new Vuex.Store({
             })
         },
         doGoogleAuth() {
-            ///window.open('http://localhost:8080/login', '_blank');
+           /*window.open('http://localhost:8080/login', '_blank');*/
             window.location.href = 'https://localhost:8080/login'
-        },
-
-
-        loadUser(context, user) {
-            console.log('USER LOADED: ' + user.toString())
-            context.commit('setCurrentUser', user)
-        },
-        userAuthorized() {
         }
     },
     plugins: [createPersistedState()]
