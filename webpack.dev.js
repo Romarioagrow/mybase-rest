84cc12fb7e89b499c9c1efe3471d2698abf45d2f
Romarioagrow <Romarioagrow@mail.ru>
2023-09-20 22:31:30 +0300

    2023 updates

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,8 +5,8 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'source-map',
     devServer: {
-        contentBase: './dist',
-        disableHostCheck: true,
+        //contentBase: './dist',
+        //disableHostCheck: true,
         compress: true,
         port: 8000,
         allowedHosts: [
